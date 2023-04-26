4ac20102121865c24441dc43e57e00e18c0ce3cf
mrodrigochaves <dev.marciorodrigo@gmail.com>
2023-04-26 09:36:41 -0300

    Finalizando execicio 01

--- src/edu/mrodrigochaves/challenge/Exercicio01/InvertePalavras.java
+++ src/edu/mrodrigochaves/challenge/Exercicio01/InvertePalavras.java
@@ -9,13 +9,13 @@ import java.util.regex.Pattern;
 public class InvertePalavras {
 
     public static String invertePalavras(String frase) {
-
+        // Dividir a string em palavras
         Pattern amostra = Pattern.compile("\\s");
         
         String[] palavras = amostra.split(frase);
         String resultado = "";
 
-
+        // Concatenando palavras na variavel "resultado"
         for (int i = 0; i < palavras.length; i++) {
             if (i == palavras.length - 1)
                 resultado = palavras[i] + resultado;
@@ -25,11 +25,12 @@ public class InvertePalavras {
         return resultado;
     }
 
+    // Invertendo ordem das palavras, adicionando CamelCase e ponto 
     public static void main(String[] args) {
         String s1 = "cachorro morde homem";
         System.out.println(invertePalavras(s1));
         s1 = s1.substring(0, 1).toUpperCase() + s1.substring(1);
-        System.out.println(s1);
+        System.out.println(s1 + ".");
 
     }
 }
