ce3ad6c051b55077f421a97d3c2dcba4f8353b77
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-04-26 21:13:05 -0300

    parte 02 do exercicio 02

--- src/edu/mrodrigochaves/challenge/Exercicio02/PosicaoPalavras.java
+++ src/edu/mrodrigochaves/challenge/Exercicio02/PosicaoPalavras.java
@@ -2,12 +2,31 @@ package edu.mrodrigochaves.challenge.Exercicio02;
 
 public class PosicaoPalavras {
 
+  public static String posicaoPenultimaPalavra( String frase) {
+    //Retorna penultima palavra da string
+    String[] palavras = frase.split(" ");
+    if(palavras.length < 2 ){
+      return " ";
+    }
+    return palavras[palavras.length - 2];
+  }
+
+  public static String posicaoPalavra(String frase, int n) {
+    //Retorna a enésima palavra da string
+    String[] palavras = frase.split(" ");
+    if(n < 1 || n > palavras.length){
+      return " ";
+    }
+    return palavras[ n - 1];
+  }
     public static void main(String[] args) {
+      //Recebe os métodos e imprime o resultado
       String s1 = "Eu sou um desenvolverdor Java";
-      String s2 = s1.substring(9,24); 
+      System.out.println(posicaoPenultimaPalavra(s1));
 
-
-      System.out.println(s2);
+      s1 = "Eu posso programar com Java";
+      int n = 2;
+      System.out.println(posicaoPalavra(s1, n));
     }
 
     
